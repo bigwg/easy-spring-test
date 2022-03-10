3ed3163c62f4dbe2ae17d1f3c89319298d5f456f
zhaozhiwei <zhaozhiwei04@meituan.com>
2022-03-10 17:35:40 +0800

    v1.0.0

---
+++ src/main/java/com/zzw/easy/spring/test/context/AutoImportsContextCustomizerFactory.java
@@ -0,0 +1,42 @@
+package com.zzw.easy.spring.test.context;
+
+import com.zzw.easy.spring.test.annotation.AutoImport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.test.context.ContextConfigurationAttributes;
+import org.springframework.test.context.ContextCustomizer;
+import org.springframework.test.context.ContextCustomizerFactory;
+import org.springframework.util.Assert;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+/**
+ * TODO
+ *
+ * @author zhaozhiwei
+ * @since 2022/3/3
+ */
+public class AutoImportsContextCustomizerFactory implements ContextCustomizerFactory {
+
+    @Override
+    public ContextCustomizer createContextCustomizer(Class<?> testClass,
+                                                     List<ContextConfigurationAttributes> configAttributes) {
+        if (AnnotatedElementUtils.findMergedAnnotation(testClass, AutoImport.class) != null) {
+            assertHasNoBeanMethods(testClass);
+            return new AutoImportsContextCustomizer(testClass);
+        }
+        return null;
+    }
+
+    private void assertHasNoBeanMethods(Class<?> testClass) {
+        ReflectionUtils.doWithMethods(testClass, this::assertHasNoBeanMethods);
+    }
+
+    private void assertHasNoBeanMethods(Method method) {
+        Assert.state(!AnnotatedElementUtils.isAnnotated(method, Bean.class),
+                "Test classes cannot include @Bean methods");
+    }
+
+}
